a735359c5edcecfcbcfdc4ddd92d1373e2dc4bbf
jeangomes13 <sjean7681@gmail.com>
2022-11-02 10:44:41 -0300

    How Output Buffer work

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 
+$data = ['name' => 'Jean', 'age' => 25];
+ob_start();
+
 echo 'Home';
+
+extract($data);
+
+require 'home.php';
+
+$content = ob_get_contents();
+
+ob_end_clean();
+
+var_dump('content: ', $content);
